ab17b4d3a302ff4ec08c67a5d485a4c246cc3281
Jonathan Cable <me@joncable.com>
2023-05-30 01:55:59 -0500

    Update all npm dependencies to most recent versions

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js(['resources/assets/js/app.js'],
-    'public/js/app.js');
+    'public/js/app.js').vue();
 
 mix.sass('resources/assets/sass/app.scss',
     'public/css/app.css');
